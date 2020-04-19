6a9826980717febb7e97373393054deaaa25aa95
portasynthinca3 <portasynthinca3@gmail.com>
2020-04-19 19:04:56 +0300

    add support for images with no alpha channel

--- src/examples/bot.py
+++ src/examples/bot.py
@@ -18,7 +18,7 @@ def run():
     tl_y = int(sys.argv[3])
     # read the image
     img = cv2.imread(path, cv2.IMREAD_UNCHANGED)
-    (sz_y, sz_x, _) = img.shape
+    (sz_y, sz_x, channels) = img.shape
     print('Read image with size: ' + str((sz_x, sz_y)))
     print('Estimated draw time: ' + str(math.ceil(sz_x * sz_y * 4 / 60)) + ' minutes')
     # create the ppfun API class
@@ -28,7 +28,10 @@ def run():
     # make a list of pixels
     for y in range(sz_y):
         for x in range(sz_x):
-            choice_list.append((x, y))
+            if channels == 3:
+                choice_list.append((x, y))
+            elif img[y, x][3] > 64:
+                choice_list.append((x, y))
     # draw the image
     while len(choice_list) > 0:
         # pick a random pixel
@@ -39,20 +42,24 @@ def run():
         while canv.remaining_cooldown() >= 50:
             pass
         # get pixel color
-        (b, g, r, a) = img[y, x]
-        # place a pixel if it's not fully transparent
-        if a > 64:
-            trying = True
-            while trying:
-                try:
-                    canv.set_pixel((tl_x + x, tl_y + y), canv.approx_color((r, g, b)))
-                    trying = False
-                except Exception as e:
-                    print('An error occured. You probably need to enter Captcha in your browser. Place a pixel somewhere manually, then return here and hit Enter.\nError code: ' + str(e))
-                    print('Pixel: ' + str((x, y)) + ', color: ' + str((r, g, b, a)) + ', color index: ' + str(canv.approx_color((r, g, b))))
-                    input()
-            print('Placed a pixel at ' + str((tl_x + x, tl_y + y)) + ' - ' +
-                  '{:5.2f}'.format(100 - (100 * len(choice_list) / (sz_x * sz_y))) + '% - cooldown: ' + '{:4.1f}'.format(canv.remaining_cooldown()) + ' s')
+        if channels == 4:
+            (b, g, r, a) = img[y, x]
+        else:
+            (b, g, r) = img[y, x]
+            a = 255
+        # place it
+        trying = True
+        while trying:
+            try:
+                canv.set_pixel((tl_x + x, tl_y + y), canv.approx_color((r, g, b)))
+                trying = False
+            except Exception as e:
+                print('An error occured. You probably need to enter Captcha in your browser. Place a pixel somewhere manually, then return here and hit Enter.\nError code: ' + str(e))
+                print('Pixel: ' + str((x, y)) + ', color: ' + str((r, g, b, a)) + ', color index: ' + str(canv.approx_color((r, g, b))))
+                input()
+        # print some info
+        print('Placed a pixel at ' + str((tl_x + x, tl_y + y)) + ' - ' +
+              '{:5.2f}'.format(100 - (100 * len(choice_list) / (sz_x * sz_y))) + '% - cooldown: ' + '{:4.1f}'.format(canv.remaining_cooldown()) + ' s')
 
 if __name__ == "__main__":
     run()
